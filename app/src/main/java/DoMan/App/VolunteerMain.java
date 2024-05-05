95b1bce46b22dc8c9ba2ac5259d9095219f4435e
Bryan Javelosa <javelosabryan14@gmail.com>
2024-05-05 14:23:44 +0800

    Your descriptive commit message here

---
+++ app/src/main/java/DoMan/App/VolunteerMain.java
@@ -0,0 +1,14 @@
+package DoMan.App;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VolunteerMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer_main);
+    }
+}
